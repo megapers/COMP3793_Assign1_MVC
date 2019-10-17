1d741ad88b3f1790adaf5255f165517d65964386
Timur Makimov <megapers@mail.ru>
2019-10-17 00:49:16 -0700

    GetBookById(int id) definition

--- Controllers/Api/BooksController.cs
+++ Controllers/Api/BooksController.cs
@@ -26,9 +26,28 @@ namespace Assign1.Controllers.Api
 
         // GET: api/Books/5
         [HttpGet("{id}", Name = "Get")]
-        public string GetBookById(int id)
+        public async Task<IActionResult> GetBookById(int id)
         {
-            return "value";
+            var items = await getJtokenListAsync();
+            var volumeInfo = items.Select(i => i["volumeInfo"]).ToList();       
+            var theBook = volumeInfo[id].Children().ToList();
+            var title = theBook[0].First.ToString();
+            var authors = theBook[1].First.ToString();
+            var publisher = theBook[2].First.ToString();
+            var published = theBook[3].First.ToString();
+            var description = theBook[4].First.ToString();
+
+            JArray array = new JArray();
+            array.Add(theBook[0].First);
+            array.Add(theBook[1].First);
+            array.Add(theBook[2].First);
+            array.Add(theBook[3].First);
+            array.Add(theBook[4].First);
+
+            JObject o = new JObject();
+            o["MyArray"] = array;
+            string json = o.ToString();
+            return Ok(json);
         }
 
 
